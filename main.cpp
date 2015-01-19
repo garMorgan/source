90090c3f951a9b9f94de348683b3766f8a7d27c4
garMorgan <g278m394@ku.edu>
2015-01-19 10:43:19 -0600

    First pass

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World\n";
+	cout << "Test of Mac\n";
+	string a;
+	cout << "Enter Name\n";
+	cin >> a;
+	cout << "Welcome " << a;
+	int b [6];
+	b [0] = 5;
+	return 0;
+}
